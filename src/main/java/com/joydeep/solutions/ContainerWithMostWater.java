01a80bcc0638dfc4d133b988e30c0f19c174a7e9
Shubham Jain <Jain@NSLHUB.COM>
2020-10-05 15:33:42 +0530

    structure changes for multiple languages

--- src/main/java/com/joydeep/solutions/ContainerWithMostWater.java
+++
@@ -1,9 +0,0 @@
-package com.joydeep.solutions;
-
-public class ContainerWithMostWater {
-
-//    https://leetcode.com/problems/container-with-most-water/
-    public int maxArea(int[] height) {
-        return 0;
-    }
-}
